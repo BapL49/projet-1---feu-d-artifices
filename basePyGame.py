08221691d10977d551e62268a77de249ceb67e15
BapL49 <118617986+BapL49@users.noreply.github.com>
2023-11-07 19:48:26 +0100

    déplacement de la logique principale du code dans une fonction main

--- basePyGame.py
+++ basePyGame.py
@@ -4,57 +4,62 @@ from pygame.locals import *
 import time
 
 
-pygame.init()
+def main():
 
-BLANC = (255, 255, 255)
-NOIR = (0, 0, 0)
-BLEU = (0, 0, 255)
-ROUGE = (255, 0, 0)
-VERT = (0, 255, 0)
+    pygame.init()
 
-fenetre = display.set_mode((800,500))
-display.set_caption("Feu d'artifice")
+    BLANC = (255, 255, 255)
+    NOIR = (0, 0, 0)
+    BLEU = (0, 0, 255)
+    ROUGE = (255, 0, 0)
+    VERT = (0, 255, 0)
 
-
-mouse.set_cursor(cursors.diamond)
-
-fenetre_rect = fenetre.get_rect()
-image = pygame.image.load("image/Icone.jpg")
-image_rect = image.get_rect()
-image_rect.center = fenetre_rect.center
-image = pygame.transform.scale(image, (fenetre_rect.width, fenetre_rect.height))
-fenetre.blit(image, (0, 0))
-pygame.display.flip()
+    fenetre = display.set_mode((800,500))
+    display.set_caption("Feu d'artifice")
 
 
-debut = time.time()
+    mouse.set_cursor(cursors.diamond)
 
-
-
-circle_x = 755
-circle_y = 410
-circle_radius = 30 
-
-
-while time.time() - debut < 5:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-
-continuer = True
-while continuer :
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            continuer = False
-
-    fenetre.fill(BLANC)
-    sol = pygame.draw.rect(fenetre, NOIR, (0, 480, 800, 20))
-    tourelle_square = pygame.draw.rect(fenetre,VERT, (730, 430, 50, 50))
-    tourrelle_circle = pygame.draw.circle(fenetre, VERT, (circle_x, circle_y), circle_radius )
-    tourrelle_canon = pygame.draw.line(fenetre, VERT, (circle_x - 50, circle_y - 35), (circle_x, circle_y), 10)
-
+    fenetre_rect = fenetre.get_rect()
+    image = pygame.image.load("image/Icone.jpg")
+    image_rect = image.get_rect()
+    image_rect.center = fenetre_rect.center
+    image = pygame.transform.scale(image, (fenetre_rect.width, fenetre_rect.height))
+    fenetre.blit(image, (0, 0))
     pygame.display.flip()
 
 
-pygame.quit()
+    debut = time.time()
+
+
+
+    circle_x = 755
+    circle_y = 410
+    circle_radius = 30 
+
+
+    while time.time() - debut < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+    continuer = True
+    while continuer :
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                continuer = False
+
+        fenetre.fill(BLANC)
+        sol = pygame.draw.rect(fenetre, NOIR, (0, 480, 800, 20))
+        tourelle_square = pygame.draw.rect(fenetre,VERT, (730, 430, 50, 50))
+        tourrelle_circle = pygame.draw.circle(fenetre, VERT, (circle_x, circle_y), circle_radius )
+        tourrelle_canon = pygame.draw.line(fenetre, VERT, (circle_x - 50, circle_y - 35), (circle_x, circle_y), 10)
+
+        pygame.display.flip()
+
+
+    pygame.quit()
+
+
+main()

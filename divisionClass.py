2e830c9dc04fd29b8cad87d427f0ac6514f7713b
BapL49 <118617986+BapL49@users.noreply.github.com>
2023-11-07 15:44:57 +0100

    initialisation de la classe division pour chaque division du feu d'artifice

---
+++ divisionClass.py
@@ -0,0 +1,8 @@
+
+
+class Division:
+    def __init__(self, positionX, positionY, vitesse, direction):
+        self.positionX = positionX
+        self.positionY = positionY
+        self.vitesse = vitesse
+        self.direction = direction

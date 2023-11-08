59d7cf67a34592797f076b464b09ddf234ad93dc
BapL49 <118617986+BapL49@users.noreply.github.com>
2023-11-08 13:43:49 +0100

    mise a jour class division

--- divisionClass.py
+++ divisionClass.py
@@ -1,9 +1,9 @@
 
 
 class Division:
-    def __init__(self, positionX, positionY, vitesse, direction):
+    def __init__(self, positionX, positionY, direction):
         self.positionX = positionX
         self.positionY = positionY
-        self.vitesse = vitesse
+        self.vitesse = 10 # entier représentant la vitesse de la division
         self.direction = direction
         self.couleur = (255, 0, 0)

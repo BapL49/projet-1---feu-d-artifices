35447ef5a9625f344fc210d72054345f032cf44c
Jean Onillon <jean.onillon49@gmail.com>
2023-11-09 08:55:02 +0100

    Commentaire

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ from pygame import *
 from pygame.locals import *
 import time
 
-
+#Fonction Principale
 def main():
-
+    #Initialisation de pygame
     pygame.init()
 
+    #Variable de couleur
     BLANC = (255, 255, 255)
     NOIR = (0, 0, 0)
     BLEU = (0, 0, 255)
     ROUGE = (255, 0, 0)
     VERT = (0, 255, 0)
 
+    #Création de la fênetre en indiquant la taille et le nom
     fenetre = display.set_mode((800,500))
     display.set_caption("Feu d'artifice")
 
-
+    #Changement de la forme du curseur a l'intérieur de la fênetre
     mouse.set_cursor(cursors.diamond)
 
+    #Création de l'image de départ
     fenetre_rect = fenetre.get_rect()
     image = pygame.image.load("image/Icone.jpg")
     image_rect = image.get_rect()
@@ -32,25 +35,27 @@ def main():
     debut = time.time()
 
 
-
+    #Valeur du cercle de la tourelle
     circle_x = 755
     circle_y = 380
     circle_radius = 30 
 
-
+    #Boucle d'attente avant l'apparition de la page Principale
     while time.time() - debut < 5:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-
+    #Boucle Continue de Jeeu
     continuer = True
     while continuer :
         for event in pygame.event.get():
             if event.type == QUIT:
                 continuer = False
 
+
         fenetre.fill(BLANC)
+        #Dessin du sol et de la tourelle
         sol = pygame.draw.rect(fenetre, NOIR, (0, 440, 800, 20))
         tourelle_square = pygame.draw.rect(fenetre,VERT, (730, 400, 50, 50))
         tourrelle_circle = pygame.draw.circle(fenetre, VERT, (circle_x, circle_y), circle_radius )

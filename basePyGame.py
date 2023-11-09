9fbd62db2008f9542c989edb5c581308bd45cca2
Miguel <miguelangelreyesmejia30@gmail.com>
2023-11-09 09:08:35 +0100

    code avec le bouton pour fermer la fenetre

--- basePyGame.py
+++ basePyGame.py
@@ -4,20 +4,20 @@ from pygame.locals import *
 import time
 
 
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+BLEU = (0, 0, 255)
+ROUGE = (255, 0, 0)
+VERT = (0, 255, 0)
+
 def main():
 
     pygame.init()
+    
 
-    BLANC = (255, 255, 255)
-    NOIR = (0, 0, 0)
-    BLEU = (0, 0, 255)
-    ROUGE = (255, 0, 0)
-    VERT = (0, 255, 0)
-
+    #creer la fenetre
     fenetre = display.set_mode((800,500))
     display.set_caption("Feu d'artifice")
-
-
     mouse.set_cursor(cursors.diamond)
 
     fenetre_rect = fenetre.get_rect()
@@ -28,47 +28,46 @@ def main():
     fenetre.blit(image, (0, 0))
     pygame.display.flip()
 
-
-    debut = time.time()
-
+    angle = 45
 
 
     circle_x = 755
     circle_y = 380
     circle_radius = 30 
+    debut = time.time()
 
-angle = 45
+    while time.time() - debut < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
+    #bouton pour fermer le jeu
+    quit_surface=pygame.image.load("image/redx.png")
+    quit_surface=pygame.transform.scale(quit_surface,(20,20))
+    quit_rect = quit_surface.get_rect()
 
+    continuer = True
 
+    while continuer :
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                continuer = False
+            if event.type == MOUSEBUTTONUP:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if quit_rect.collidepoint(mouse_x,mouse_y):
+                    continuer=False
 
+        fenetre.fill(BLANC)
+        sol = pygame.draw.rect(fenetre, NOIR, (0, 480, 800, 20))
+        tourelle_square = pygame.draw.rect(fenetre,VERT, (730, 430, 50, 50))
+        tourrelle_circle = pygame.draw.circle(fenetre, VERT, (circle_x, circle_y), circle_radius )
+        tourrelle_canon = pygame.draw.line(fenetre, VERT, (circle_x - 50, circle_y - 35), (circle_x, circle_y), 10)
+        fenetre.blit(quit_surface,(0,0),)
 
-while time.time() - debut < 5:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-#bouton pour fermer le jeu
-quit_surface=pygame.image.load("image/redx.png")
-quit_surface=pygame.transform.scale(quit_surface,(20,20))
-quit_rect = image.get_rect()
-
-continuer = True
-while continuer :
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            continuer = False
-
-    fenetre.fill(BLANC)
-    sol = pygame.draw.rect(fenetre, NOIR, (0, 480, 800, 20))
-    tourelle_square = pygame.draw.rect(fenetre,VERT, (730, 430, 50, 50))
-    tourrelle_circle = pygame.draw.circle(fenetre, VERT, (circle_x, circle_y), circle_radius )
-    tourrelle_canon = pygame.draw.line(fenetre, VERT, (circle_x - 50, circle_y - 35), (circle_x, circle_y), 10)
-    fenetre.blit(quit_surface,(0,0),)
-
-    pygame.display.flip()
+        pygame.display.flip()
 
     pygame.quit()
 
 
-main()
+if __name__ == "__main__":
+    main()

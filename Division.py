e42defec84e6de7fcf995fafb05b03c900e7f02e
BapL49 <118617986+BapL49@users.noreply.github.com>
2023-12-05 15:21:38 +0100

    supression de l'attributs vitesse

--- Division.py
+++ Division.py
@@ -10,7 +10,6 @@ class Division:
         self.direction = direction # angle (en radian) vers lequel se dirige la division
         self.vitesseInitialeBase = 6 # constante vitesse de départ de la division (commune à toutes les divisions)
         self.vitesseInitialeY = self.vitesseInitialeBase * math.sin(self.direction) # vitesse de départ sur l'axe Y
-        self.vitesse = randint(8, 22) # vitesse de la division (pour l'axe Y et X)
         self.acceleration = 9.8 # constante d'accélération 
         self.couleur = (255, 0, 0)
         self.circle_radius = randint(5, 15) # circonférence de la division
@@ -25,10 +24,10 @@ class Division:
     def move(self):
         dt = 0.1 # delta time (valeur fixe)
         self.positionY += 0.5 * self.acceleration * dt**2 + self.vitesseInitialeY * dt
-        self.positionX += 0.5 * self.acceleration * dt**2 + self.vitesse * self.vitesseInitialeBase * math.cos(self.direction) * dt
+        self.positionX += 0.5 * self.acceleration * dt**2 + self.acceleration * self.vitesseInitialeBase * math.cos(self.direction) * dt
         # met à jour la position de la division
         self.circle = pygame.draw.circle(self.fenetre, self.couleur, (int(self.positionX), int(self.positionY)), self.circle_radius)
-        self.vitesseInitialeY += self.vitesse # augmente la vitesse sur l'axe Y
+        self.vitesseInitialeY += self.acceleration # augmente la vitesse sur l'axe Y
 
 
     def update(self):
